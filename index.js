ba980948542577b23befeb23792701997215b0c8
barbara prusiewicz <barbara.prusiewicz@gmail.com>
2019-06-22 04:43:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function printString(myString) {
   if (myString.length > 1)
     printString(myString.substring(1))
 }
+
+function reverseString(str) {
+  if (str.length > 0)
+    return reverseString(str.substring(1)) + str[0]
+  else {
+    return ""
+  }
+}
